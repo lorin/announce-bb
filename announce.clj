d3a9771d156ea8d584158f685d265c4888c68436
Lorin Hochstein <lhochstein@netflix.com>
2022-01-10 21:43:11 -0800

    checkpoint

--- announce.clj
+++ announce.clj
@@ -9,20 +9,19 @@
     announce off")
 
 
+  
+(def announce-the-time-domain "com.apple.speech.synthesis.general.prefs")
+(def announce-the-time-key "TimeAnnouncementPrefs")
+(def announce-the-time-enabled-key "TimeAnnouncementsEnabled")
+
 (defn set-boolean-pref
 "Set a boolean nested preference"
   [domain-name key nested-key flag]
     (let [domain (str (System/getProperty "home") "/Library/Preferences/" domain-name)]
     (sh "defaults" "write" domain key "-dict-add" nested-key "-bool" (if flag "YES" "NO"))))
-  
-(def announce-the-time-domain "com.apple.speech.synthesis.general.prefs")
-(def announce-the-time-key "TimeAnnouncementPrefs")
-(def announce-the-time-enabled-key "TimeAnnouncementsEnabled")
-(def set-or-unset-announce-the-time-flag (partial set-boolean-pref announce-the-time-domain announce-the-time-key announce-the-time-enabled-key))
 
+(def set-announce-the-time-flag (partial set-boolean-pref announce-the-time-domain announce-the-time-key announce-the-time-enabled-key))
 
-(def set-announce-the-time-pref #(set-or-unset-announce-the-time-flag true))
-(def unset-announce-the-time-pref #(set-or-unset-announce-the-time-flag true))
 
 (defn start-service
   [service-name]
@@ -36,23 +35,18 @@
         service (str "gui/" uid "/" service-name)]
     (sh "launchctl" "kill" "SIGTERM" service)))
   
-(defn start-speech-synthesis-server
-  []
-  (start-service "com.apple.speech.synthesisserver"))
+(def speech-synthesis-server-name "com.apple.speech.synthesisserver")
 
-(defn stop-speech-synthesis-server
-  []
-  (stop-service "com.apple.speech.synthesisserver"))
 
 (defn turn-on
   []
-  (set-announce-the-time-pref)
-  (start-speech-synthesis-server))
+  (set-announce-the-time-flag true)
+  (start-service speech-synthesis-server-name))
 
 (defn turn-off
   []
-  (unset-announce-the-time-pref)
-  (stop-speech-synthesis-server))
+  (set-announce-the-time-flag false)
+  (stop-service speech-synthesis-server-name))
   
 (defn print-usage
   []
