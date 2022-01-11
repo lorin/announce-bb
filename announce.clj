e1a09350f7d0355e7fdf9f9ed4abe1d4768218c9
Lorin Hochstein <lhochstein@netflix.com>
2022-01-10 21:38:15 -0800

    before changing impl

--- announce.clj
+++ announce.clj
@@ -6,35 +6,54 @@
 (def usage "Usage:
 
     announce on
-    announce off
-    announce status")
+    announce off")
+
+(defn print-usage 
+  []
+  (println usage))
 
 (defn expand-home [s]
   (if (.startsWith s "~")
     (replace-first s "~" (System/getProperty "user.home"))
     s))
 
+
+(defn set-boolean-pref
+"Set a boolean nested preference"
+  [domain-name key nested-key flag]
+    (let [domain (str (System/getProperty "home") "/Library/Preferences/" domain-name)]
+    (sh "defaults" "write" domain key "-dict-add" nested-key "-bool" (if flag "YES" "NO"))))
+  
+
 (defn set-pref
   "Set the value in a nested key"
   [domain-name key nested-key]
-  (let [domain (str (System/getProperty "home") "/Library/Preferences/" domain-name)]
-    (sh "defaults" "write" domain key "-dict-add" nested-key "-bool" "YES")))
+  (set-boolean-pref domain-name key nested-key true))
 
 
 (defn unset-pref
-  "Set the value in a nested key"
+  "Clear the value in a nested key"
   [domain-name key nested-key]
-  (let [domain (str (System/getProperty "home") "/Library/Preferences/" domain-name)]
-    (sh "defaults" "write" domain key "-dict-add" nested-key "-bool" "NO")))
+    (set-boolean-pref domain-name key nested-key false))
+
+(def announce-the-time-domain "com.apple.speech.synthesis.general.prefs")
+(def announce-the-time-key "TimeAnnouncementPrefs")
+(def announce-the-time-enabled-key "TimeAnnouncementsEnabled")
+
+
+(def set-or-unset-announce-the-time-flag (partial set-boolean-pref announce-the-time-domain announce-the-time-key announce-the-time-enabled-key))
+
 
 (defn set-announce-the-time-pref
   []
-  (set-pref "com.apple.speech.synthesis.general.prefs" "TimeAnnouncementPrefs" "TimeAnnouncementsEnabled"))
+  (set-pref announce-the-time-domain announce-the-time-key announce-the-time-enabled-key))
 
 (defn unset-announce-the-time-pref
   []
-  (unset-pref "com.apple.speech.synthesis.general.prefs" "TimeAnnouncementPrefs" "TimeAnnouncementsEnabled"))
+  (unset-pref announce-the-time-domain announce-the-time-key announce-the-time-enabled-key))
 
+;(def set-announce-the-time-pref #(set-or-unset-announce-the-time-flag true))
+;(def unset-announce-the-time-pref #(set-or-unset-announce-the-time-flag true))
 
 (defn start-service
   [service-name]
@@ -67,18 +86,13 @@
   (stop-speech-synthesis-server))
   
 
-(defn check-status
-  []
-  (println "check the status!"))
-
 (defn main
   [args]
   (if-let [[arg] args]
     (case arg
       "on" (turn-on)
       "off" (turn-off)
-      "status" (check-status)
-      (println usage))
-    (println usage)))
+      (print-usage))
+    (print-usage)))
 
 (main *command-line-args*)
