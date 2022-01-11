a1cef9f41abeb7a013d467497e1a794f1a7326c7
Lorin Hochstein <lhochstein@netflix.com>
2022-01-10 20:33:37 -0800

    checkpoint

--- announce.clj
+++ announce.clj
@@ -1,11 +1,32 @@
 #!/usr/bin/env bb
 (ns announce)
 
-(def usage "announce.
+(def usage "Usage:
 
-Usage:
     announce on
     announce off
     announce status")
 
-(println usage)
+(defn turn-on
+  []
+  (println "turn it on!"))
+
+(defn turn-off
+  []
+  (println "turn it off!"))
+
+(defn check-status
+  []
+  (println "check the status!"))
+
+(defn main
+  [args]
+  (if-let [[arg] args]
+    (case arg
+      "on" (turn-on)
+      "off" (turn-off)
+      "status" (check-status)
+      (println usage))
+    (println usage)))
+
+(main *command-line-args*)
